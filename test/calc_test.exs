728de26d078892a92442f0c647a627600305912a
Everaldo da costa filho <everaldo.webmail@gmail.com>
2021-02-11 08:40:10 -0300

    Mais tests

--- test/calc_test.exs
+++ test/calc_test.exs
@@ -17,13 +17,13 @@ defmodule CalcTest do
   #   end
   # end
 
-  # describe "subtract/2" do
-  #   test "Success" do
-  #     for [x, y] <- @data_provider do
-  #       assert (x - y) == Calc.subtract(x, y)
-  #     end
-  #   end
-  # end
+  describe "subtract/2" do
+    test "Success" do
+      for [x, y] <- @data_provider do
+        assert (x - y) == Calc.subtract(x, y)
+      end
+    end
+  end
 
   describe "divide/2" do
     test "Success" do
