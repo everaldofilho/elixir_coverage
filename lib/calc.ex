302288910ff9c8df7cf5d4db2d87a2f0c42e9413
Everaldo da costa filho <everaldo.webmail@gmail.com>
2021-02-11 20:00:37 -0300

    test

--- lib/calc.ex
+++ lib/calc.ex
@@ -1,12 +1,12 @@
 defmodule Calc do
 
-  def sum(x, y), do: x + y
+def sum(x, y), do: x + y
 
-  def subtract(x, y), do: x - y
+    def subtract(x, y), do: x - y
 
   def divide(x, y), do: x / y
 
-  def multiply(x, y), do: x * y
+def multiply(x, y), do: x * y
 
     def sum_total(items), do: Enum.sum(items)
 end
